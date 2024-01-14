9ef088bf793b8b0f268da7f1da7b913e0d0bea02
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-01-15 03:08:16 +0530

    Add models and database connection files

---
+++ Backend/src/models/index.js
@@ -0,0 +1,9 @@
+const { Alumni } = require("./alumniModel")
+const {Admin } = require("./adminModel")
+const {Collage} = require("./collegeModel")
+
+module.exports={
+    Alumni,
+    Admin,
+    Collage
+}
