e52094c51c27a6f88c327c52bd63d31062a32c99
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-01-17 01:24:34 +0530

    Add react-icons package and update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Dropdown from './Components/Dropdown'
+import Home from './Components/Home'
+import Topbar from './Components/Topbar'
 import HomeCard from './Components/HomeCard'
 import Navbar from './Components/Navbar'
 import Login from './pages/Login'
@@ -10,9 +12,10 @@ function App() {
   return (
     <>
     {/* <Dropdown/> */}
-      <Navbar/>
-      {/* <Topbar/> */}
+      {/* <Navbar/> */}
+      <Topbar/>
     <Login/>
+    {/* <Home/> */}
      {/* <HomeCard/> */}
      
     </>
