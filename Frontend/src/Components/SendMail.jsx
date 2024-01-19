a70c6218d2b7a5c9f9124bb18d3428d491cea8ad
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-01-19 23:07:47 +0530

    Update SendMail component UI

--- Frontend/src/Components/SendMail.jsx
+++ Frontend/src/Components/SendMail.jsx
@@ -16,7 +16,11 @@ const SendMail = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <form onSubmit={(e) => e.preventDefault()} className="w-full max-w-md">
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="px-8 pt-6 pb-8 mb-4">
+        <h2 className="my-3 text-center text-2xl font-bold text-gray-900">
+  Send Mail to Students
+</h2>
+
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               College Name
@@ -82,12 +86,13 @@ const SendMail = () => {
             />
           </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          <div className="flex items-center justify-center">
+           <button
               type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-black-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Submit
+              {/* <AcademicCapIcon className="h-5 w-5 text-white  group-hover:text-indigo-400 " aria-hidden="true" /> */}
+               Send Mail
             </button>
           </div>
         </div>
