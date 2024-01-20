ca8a5b45967ec6d42fc15569121f7e75e3ff0eb6
sahilali88084667@gmail.com <sahilali88084667@gmail.com>
2024-01-20 14:44:14 +0530

    Refactor login handling and add reusable NotLoggedIn component

--- Frontend/src/Components/Event.jsx
+++ Frontend/src/Components/Event.jsx
@@ -3,17 +3,10 @@ import { getLoggedIn } from '../services/authService';
 import { Link } from 'react-router-dom';
 import NotLoggedIn from './NotLoggedIn';
 import { ToastContainer, toast } from 'react-toastify';
-import noDataIllustration from '../assets/img/no_data.svg';  // Replace with the actual path
+// Replace with the actual path
 
 function Event() {
   const loggedIn = getLoggedIn();
-
-  useEffect(() => {
-    // If not logged in, show a toast
-    if (!loggedIn) {
-      toast.error('Please log in to access this page');
-    }
-  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -21,7 +14,7 @@ function Event() {
         <h1 className="text-4xl font-bold mb-4">Event</h1>
       ) : (
         <>
-          <img src={noDataIllustration} alt="No Data Illustration" className="mt-4 h-20 w-20" />
+          
           <NotLoggedIn text="Event" />
         </>
       )}
--- Frontend/src/Components/NotLoggedIn.jsx
+++ Frontend/src/Components/NotLoggedIn.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { getLoggedIn } from '../services/authService';
-// import { toast, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
+import noDataIllustration from '../assets/img/no_data.svg';  
+import { useEffect } from 'react';
 
 
 
 function NotLoggedIn(props) {
-    // toast.error('Please log in to access this page');
+    useEffect(() => {
+        // If not logged in, show a toast
+
+          toast.error('Please log in to access this page');
+        
+      }, []);
   return (
     <>
-    {/* <ToastContainer/> */}
+     
+      <img src={noDataIllustration} alt="No Data Illustration" className="flex items-center h-20 w-20" />
       <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">You're Not Logged In</h1>
           <p className="text-gray-600 mb-4">Please log in to access our {props.text} Tab.</p>
